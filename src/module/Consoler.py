07ee42a9a1a4a8bb882b1d08f670bb7597f9a940
WallKing110 <ksalamahin2004@mail.ru>
2024-03-18 15:12:10 +0500

    Обновил модуль Consoler.py.

--- src/module/Consoler.py
+++ src/module/Consoler.py
@@ -1,6 +1,8 @@
 import os
 from sys import platform
 from re import findall
+from socket import gethostbyname
+from socket import gaierror
 
 
 class Consoler(object):
@@ -18,7 +20,14 @@ class Consoler(object):
         self.output = self.capture_command(self.ip)
         self.check_succeed(self.output)
 
+    @staticmethod
+    def get_ip_from_domain(domain):
+        '''domain -> ip'''
+        ip = gethostbyname(domain)
+        return ip
+
     def capture_command(self, ip):
+        '''ip -> текст команды по трассировке'''
         p = os.popen(self.command + ' ' + ip)
         output = p.readlines()
         p.close()
@@ -26,26 +35,49 @@ class Consoler(object):
         return output
 
     def check_succeed(self, text):
+        '''Метод берет на вход текст команды traceroute и поднимает
+ошибку, если не удалось дотрассироваться до конечного хоста'''
         if self.system == 'linux':
             end_of_incmpt_out = '* * */n'
             index_of_incmplt_out = text.rfind(end_of_incmplt_out)
             if index_of_incmplt_out == len(end_of_incmplt_out):
                 raise UnreachableHostException(self.ip)
         elif self.system == 'win':
-            len_of_ending_string = 26
+            len_of_ending_string = 27
             max_len_of_ipv4 = 20
-            if text.rfind(self.ip) < len_of_ending_string + max_len_of_ipv4 + 1:
+            if text.rfind(self.ip) < len_of_ending_string + max_len_of_ipv4:
                 raise UnreachableHostException(self.ip)
-        
+
+    @staticmethod
+    def is_ip(text):
+        '''Метод берет на вход текст и возвращает True если этот текст ipv4
+без маски, либо False, если нет.'''
+        ip_pattern = r'\d+\.\d+\.\d+\.\d+'
+        ip_found = findall(ip_pattern, text)
+        if len(ip_found) != 1:
+            return False
+        if text == ip_found[0]:
+            return True
+        return False
+
+    @staticmethod
+    def is_domain(text):
+        '''Метод берет на вход текст и возвращает True если из этого текста
+можно получить ipv4 адрес, либо выбрасывает исключение socket.gaierror'''
+        ip = gethostbyname(text)
+        return True
+
 
 class UnreachableHostException(Exception):
+    '''Класс-исключение, которое предназначено для сигнализирования о том,
+что не удалось добраться до конечного пункта трассировки'''
     def __init__(self, ip):
         self.ip = ip
 
     def __str__(self):
-        return 'Не получилось добраться до хоста {1},'\
+        return 'Не получилось добраться до хоста {0},'\
                'Попробуйте другой ip-адрес.'.format(self.ip)
 
+
 if __name__ == '__main__':
-    c = Consoler('8.8.8.8')
-    print (c.output)
+    pass
